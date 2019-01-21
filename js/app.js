cc4f2767cdf453ef315d167f80fe6a7d5075c586
Aeden <hauguel.frederic@gmail.com>
2019-01-21 17:33:34 +0100

    Tentative de récupération avis Facebook avec Vue.js

--- js/app.js
+++ js/app.js
@@ -25,44 +25,10 @@ var description = new Vue({
     data: {
         description: null,
     },
-    created: function () {
-        window.fbAsyncInit = function () {
-            FB.init({
-                appId: '1658021547630800',
-                autoLogAppEvents: true,
-                xfbml: true,
-                version: 'v3.2'
-            });
-
-            FB.api(
-                "/357781158359111/ratings?access_token=EAAXj9jlgYNABAC0OcbzabhVquWuCwyZAu5KF3RIZAioNG9seIQZCLvvQCeJXvdl1RygrNA3oLddKtbXDiZARaZAWsyKBnzZC6nZClFkADaHzJDLMep7sIQV0HvjQybcyAZBfgEsiAfp0cMZAi7OyYaqpNZAKJBELDdMUj5xfHnpUEc9dNhXVoOpHeLX0bovKhVuCZCyHD5pKj5OhAZDZD",
-                function (response) {
-                    console.log("truc");
-                    if (response && !response.error) {
-                    console.log(response);
-                    this.description = response;
-                    }else{
-                        console.log("truc");
-                    }
-                }
-            );
-
-
-            (function (d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) { return; }
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-
-        }
-    },
-    
     mounted(){ // Equivalent on ready en Jquery
         return axios
             .get("./data/description.json").then(response => {
-                //this.description = response.data.description
+                this.description = response.data.description
             })
     }
 })
--- js/custom.js
+++ js/custom.js
@@ -2,38 +2,34 @@
  * Code pour les avis et le chat Facebook
  */
 
-
-window.fbAsyncInit = function () {
+window.fbAsyncInit = function() {
     FB.init({
-        appId: '1658021547630800',
-        autoLogAppEvents: true,
-        xfbml: true,
-        version: 'v3.2'
+      appId            : '1658021547630800',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v2.12'
     });
 
+    
     FB.api(
-        "/357781158359111/ratings?access_token=EAAXj9jlgYNABAC0OcbzabhVquWuCwyZAu5KF3RIZAioNG9seIQZCLvvQCeJXvdl1RygrNA3oLddKtbXDiZARaZAWsyKBnzZC6nZClFkADaHzJDLMep7sIQV0HvjQybcyAZBfgEsiAfp0cMZAi7OyYaqpNZAKJBELDdMUj5xfHnpUEc9dNhXVoOpHeLX0bovKhVuCZCyHD5pKj5OhAZDZD",
+        "/357781158359111/ratings?access_token=EAAXj9jlgYNABAK1kkx9CaQVGrxKcj1YrQxYmz9qYQGN4pBztk3TvZCc8w51m2Y41XSZBb4laJB8nDdOxmNdmSgVH00nF2F0zdeFnj4mUjphFOPAa5qz41PwvkiggBoowBZBr6FpPWhLNvZBCxp2fwboIWpW5RraZCRlydeunfWafn1iCzGePS",
         function (response) {
-            console.log("truc");
             if (response && !response.error) {
             console.log(response);
-            this.description = response;
-            }else{
-                console.log("truc");
             }
         }
     );
+    
+  };
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
 
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "https://connect.facebook.net/fr_FR/sdk/xfbml.customerchat.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-
-}
 
 /**
  * Code pour la carte du site
