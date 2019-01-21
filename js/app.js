b90a846b952f30cd071eff136f270328ddcc80d6
Aeden <hauguel.frederic@gmail.com>
2019-01-21 14:16:47 +0100

    Premiers changements avec Vue.js

---
+++ js/app.js
@@ -0,0 +1,34 @@
+/**
+ * Requête simple :
+ * ----------------
+ * axios
+ * .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+ * .then(response => (this.info = response.data.bpi))
+ * .catch(error => console.log(error))
+ *
+ * Requête multiple :
+ * ------------------
+    axios.all([
+        axios.get('/api/seat/models'),
+        axios.get('/api/volkswagen/models')
+        ])
+        .then(axios.spread(function (seat, volkswagen) {
+        let vehicles = seat.data.concat(volkswagen.data);
+        this.setState({ vehicles: vehicles })
+        }))
+        //.then(response => this.setState({ vehicles: response.data }))
+        .catch(error => console.log(error));
+ */
+
+var description = new Vue ({
+    el: "#description",
+    data: {
+        description: "bonjour",
+    },
+    mounted(){ // Equivalent on ready en Jquery
+        return axios
+        .get("./data/description.json").then(response => {
+            this.description = response.data.description
+        })
+    }
+})
