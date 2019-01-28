4a33d6a6680b59cea1ada43b290f8fe09786d9f6
Aeden <hauguel.frederic@gmail.com>
2019-01-28 13:45:40 +0100

    Nouvelles modification avis (ratings), map et et menu

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    var dataDirectory = "/data"
+    var dataDirectory = "./data"
 
     /**
      * ==================================================
